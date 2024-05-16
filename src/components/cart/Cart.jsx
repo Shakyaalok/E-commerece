6689dbd51e4efbf5e30a36b9d9a4efd55eeb5f33
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-05-16 16:03:19 +0530

    cartIrems lists added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
 import './Cart.css'
+import cartItems from './cartItems';
 import {Row,Col,Button} from 'react-bootstrap';
 
 const Cart = () => {
   return (
     <div className='container shadow'>
 
-      <Row className='row p-2 justify-content-evenly d-flex'>
-
-        <Col md='4'>
-            <img src="https://images.unsplash.com/photo-1506617420156-8e4536971650?q=80&w=1423&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'" alt="" className='img-fluid rounded' /> 
-        </Col>
-
-        <Col md='4'>
-        <h5>Description</h5>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis voluptas non, magni amet rem quisquam omnis aliquid nostrum nemo minima?</p>
-        </Col>
-
-        <Col md='4'>
-          <h5 className='mb-1'>Product Name</h5>
-          <p className='text-muted'>Price: $100</p>
-          <p className='text-muted'>Quantity: 1</p>
-
-          <div className='d-flex align-items-center gap-5'>
-          <div className='d-flex gap-1 g-md-0 align-items-center plus-minus'>
-            <button className='rounded-circle border-0'><span style={{ fontSize: '2rem', lineHeight: '1' }}>-</span></button>
-            <input type="number" style={{width:'2rem' , height:'2rem'}}/>
-            <button className='rounded-circle border-0'><span style={{ fontSize: '2rem', lineHeight: '1' }}>+</span></button>
-           </div>
-           <Button variant='warning' className='text-white'>Remove</Button>
-           </div>
-        </Col>
+        {cartItems.map((product)=>(
+            <Row className='row p-2 justify-content-evenly d-flex shadow rounded'>
+            <Col md='4'>
+                <img src={product.imageUrl} alt="" className='img-fluid rounded' /> 
+            </Col>
+    
+            <Col md='4'>
+            <h5>Description</h5>
+            <p>{product.description}</p>
+            </Col>
+    
+            <Col md='4'>
+              <h5 className='mb-1'>Product Name</h5>
+              <p className='text-muted'>Price: {product.price}</p>
+              <p className='text-muted'>Quantity: {product.quantity}</p>
+    
+              <div className='d-flex align-items-center gap-5'>
+              <div className='d-flex gap-1 g-md-0 align-items-center plus-minus'>
+                <button className='rounded-circle border-0'><span style={{ fontSize: '2rem', lineHeight: '1' }}>-</span></button>
+                <input type="number" style={{width:'2rem' , height:'2rem'}}/>
+                <button className='rounded-circle border-0'><span style={{ fontSize: '2rem', lineHeight: '1' }}>+</span></button>
+               </div>
+               <Button variant='warning' className='text-white'>Remove</Button>
+               </div>
+            </Col>
+    
+       
+    
+          </Row>
+        ))}
 
    
-
-      </Row>
     </div>
   )
 }
